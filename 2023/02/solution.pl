a9259b1cead94214f76445d12f4174c2558205a2
Dan Rachou <danbanan@debian>
2023-12-02 11:21:29 +0100

    Refactoring

--- 2023/02/solution.pl
+++ 2023/02/solution.pl
@@ -10,18 +10,17 @@ sub part1 {
   my %color_limit = (red => 12, green => 13, blue => 14);
 
   my $sum;
-  
+  my $game_id = 1;
+
   while (<>) {
-    my (@pairs) = m/(\w+ \d+)|(\d+ \w+)/g;
+    my (@pairs) = m/(\d+ \w+)/g;
 
-    my ($game_id) = $pairs[0] =~ m/(\d+)/;
-    my @color_pairs = map { [ split ' ', $_ ] } grep { defined $_ } @pairs[1..$#pairs];
-
-    print Dumper \@color_pairs;
+    my @color_pairs = map { [ split ' ', $_ ] } grep { defined $_ } @pairs;
 
     my $match = first { $_->[0] > $color_limit{$_->[1]} } @color_pairs;
 
     $sum += $game_id if not defined $match;
+    $game_id++;
   }
 
   return $sum;
