210be9002a403b7a62990ae016647a1f14013e1a
Dan Rachou <dolphin-dan@dolphin-dan.dolphinics.com>
2023-12-05 09:05:03 +0100

    Day 4 part 2 in progress

--- 2023/04/solution.pl
+++ 2023/04/solution.pl
@@ -3,16 +3,36 @@ use v5.16;
 use warnings;
 use strict;
 
-use List::MoreUtils qw/duplicates/;
+use Data::Dumper;
+use List::MoreUtils qw/duplicates reduce_0/;
 
-my $sum;
+sub part1 {
+    my $sum;
 
-while (<>) {
-    my @numbers = m/(\d+)/g;
-    my @winning_numbers = duplicates @numbers[1..$#numbers];
-    $sum += 2**($#winning_numbers) if $#winning_numbers >= 0;
+    while (<>) {
+	my @numbers = m/(\d+)/g;
+	my @winning_numbers = duplicates @numbers[1..$#numbers];
+	$sum += 2**($#winning_numbers) if $#winning_numbers >= 0;
+    }
+
+    return $sum;
 }
 
-say $sum;
+sub part2 {
+    my %card_scores;
+    my @copies;
+    my @cards;
 
-# push @numbers, $1 while m/((\d+\s+)+)/g; # collects two groups
+    while (<>) {
+	my @numbers = m/(\d+)/g;
+	my @winning_numbers = duplicates @numbers[1..$#numbers];
+	$card_scores{$.} = scalar @winning_numbers;
+	push @copies, (($. + 1)..($. + $card_scores{$.}));
+    }
+
+    print Dumper \%card_scores;
+
+    return $. + scalar @copies + reduce_0 { $a + $card_scores{$b} } @copies;
+}
+
+say part2();
